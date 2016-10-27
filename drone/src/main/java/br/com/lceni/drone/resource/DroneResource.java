bd5a230bb90fc5b42544e40779f0e2998c15f690
lceni <lucas.ceni@gmail.com>
2016-10-26 23:04:16 -0200

    Organizando imports

--- drone/src/main/java/br/com/lceni/drone/resource/DroneResource.java
+++ drone/src/main/java/br/com/lceni/drone/resource/DroneResource.java
@@ -1,7 +1,5 @@
 package br.com.lceni.drone.resource;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -9,7 +7,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
